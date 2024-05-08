39dd9618b6fc33b809db79f0b762c302959e9e99
Daniel CH Tan <dtch1997@users.noreply.github.com>
2024-05-09 00:14:12 +0100

    fix style

--- smol_sae/main.py
+++ smol_sae/main.py
@@ -1,6 +1,4 @@
 # %%
-%load_ext autoreload
-%autoreload 2
 
 from transformer_lens import HookedTransformer
 from einops import *
@@ -8,9 +6,6 @@ import torch
 
 from base import Config
 from utils import get_splits, Sampler
-from anthropic import AnthropicSAE
-from vanilla import VanillaSAE
-from gated import GatedSAE
 from rainbow import RainbowSAE
 
 # %%
@@ -20,7 +15,12 @@ train, validation = get_splits()
 
 # This equates to about 65M tokens
 # This takes about 16 GB of GPU memory
-config = Config(n_buffers=500, expansion=4, buffer_size=2**17, sparsities=(0.01, 0.02, 0.04, 0.07, 0.14, 0.27, 0.52, 1.00))
+config = Config(
+    n_buffers=500,
+    expansion=4,
+    buffer_size=2**17,
+    sparsities=(0.01, 0.02, 0.04, 0.07, 0.14, 0.27, 0.52, 1.00),
+)
 sampler = Sampler(config, train, model)
 sae = RainbowSAE(config, model).cuda()
 
